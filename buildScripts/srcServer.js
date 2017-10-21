0031a24c24ab2d5aec5defe309327662fa318241
rkterungwa16 <terungwakombol@gmail.com>
2017-10-21 05:51:18 +0100

    chore:Set up database configuration

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import webpack from 'webpack';
 import path from 'path';
 import open from 'open';
+import routes from '../server/routes';
 import config from '../webpack.config.dev';
+import db from '../server/lib/db.js';
+import dbConfigJson from '../server/config.json';
 
 const port = 3000;
 const compiler = webpack(config);
 const app = express();
+const dbConfig = dbConfigJson[app.get('env')];
+
+db.connect(dbConfig.mongoUrl);
+
+// Parse incoming request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
