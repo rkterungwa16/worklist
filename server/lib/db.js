d3bd27c7fe3efe899ceee41a48448b99839ae437
rkterungwa16 <terungwakombol@gmail.com>
2017-10-23 04:19:17 +0100

    style:Edit syntax to meet style guide standard

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 
-export const connect = (url, cb) => {
-  cb = cb || function(err) {
+const connect = (url, cb) => {
+  const connectionError = (err) => {
     if (err) {
-      console.error('database connection failure: \n' + err.stack);
+      console.error(`database connection failure: \n${err.stack}`);
       process.exit(1);
     }
   };
+  cb = cb || connectionError;
   mongoose.connect(url, { safe: true }, cb);
 };
+
+export default connect;
