cfb73a1cadf05b5f97b02916c6e95acdd17f3367
rkterungwa16 <terungwakombol@gmail.com>
2017-10-28 17:08:03 +0100

    feature:Add color codes to priority

--- client/components/AddedTaskItem.jsx
+++ client/components/AddedTaskItem.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
 const TaskItem = (props) => {
+  let color;
+  console.log('THIS IS THE PROPS', props);
+  if (props.tasks.priority === 'urgent') {
+    color = 'task-cat red';
+  } else if (props.tasks.priority === 'critical') {
+    color = 'task-cat orange';
+  } else {
+    color = 'task-cat green';
+  }
   return (
     <div>
       <a
         className='collection-item black-text'
       >
-        <span className='new badge red'>4</span>
+        <span
+          className={
+            color
+          }
+        >
+          {props.tasks.priority}
+        </span>
         {props.tasks.task}
       </a>
     </div>
