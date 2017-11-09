88dcea4612f3ae0d748373e29a545eca4d7c1291
rkterungwa16 <terungwakombol@gmail.com>
2017-11-09 04:52:01 +0100

    chore:Form validation for edit profile

--- client/helper/editProfileFormValidation.js
+++ client/helper/editProfileFormValidation.js
@@ -3,6 +3,9 @@ const validateInput = (data) => {
   if (data.password !== data.confirmPassword) {
     return 'Your password and confirm password does not match';
   }
+  if (!data.currentPassword) {
+    return 'Please enter your current password';
+  }
   return true;
 };
 
--- client/index.js
+++ client/index.js
@@ -17,7 +17,6 @@ import Signup from './pages/SignupPage';
 import Login from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
 import EditProfile from './components/EditProfile';
-import Collaborator from './components/Collaborator';
 import RequireAuthRoute from './components/RequireAuthRoute';
 import AuthRoute from './components/AuthRoute';
 import NotFound from './components/NotFound';
@@ -32,7 +31,6 @@ render((
         <AuthRoute exact path='/signup' component={Signup} />
         <AuthRoute exact path='/login' component={Login} />
         <RequireAuthRoute exact path='/edit-profile' component={EditProfile} />
-        <RequireAuthRoute exact path='/collaborator' component={Collaborator} />
         <RequireAuthRoute exact path='/dashboard' component={Dashboard} />
         <Route exact path='*' component={NotFound} />
       </Switch>
