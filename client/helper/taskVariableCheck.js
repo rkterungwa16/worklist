b17a872949699e200ef0359dd7df16a8e1cb4204
rkterungwa16 <terungwakombol@gmail.com>
2017-11-17 09:56:19 +0100

    chore(js doc):Update jsdoc for helper function

--- client/helper/taskVariableCheck.js
+++ client/helper/taskVariableCheck.js
@@ -1,4 +1,8 @@
-
+/**
+ * Set color codes based on priority
+ * @param {string} priority Represents the priority of a task
+ * @return {string} string representing color codes
+ */
 export const checkPriority = (priority) => {
   if (priority === 'urgent') {
     return 'task-cat yellow';
@@ -8,6 +12,11 @@ export const checkPriority = (priority) => {
   return 'task-cat green';
 };
 
+/**
+ * Set completion status for a task
+ * @param {*} completed boolean
+ * @return {string} string representing the completion status
+ */
 export const checkCompletion = (completed) => {
   if (completed === true) {
     return 'completed';
@@ -15,6 +24,12 @@ export const checkCompletion = (completed) => {
   return '';
 };
 
+/**
+ * Update task item with date from state and props
+ * @param {obj} stateDueDate date object set in the state
+ * @param {obj} propsDueDate date object from the props
+ * @return {obj} object represent either state date or props date if available
+ */
 export const checkStateDueDate = (stateDueDate, propsDueDate) => {
   if (stateDueDate) {
     return stateDueDate;
